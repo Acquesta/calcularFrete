f2ddd2f76364c7c6354e69e607ab874c197dbf5e
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2023-03-28 12:44:05 -0300

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,63 +1,85 @@
-import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { Component } from "@angular/core";
+import { ToastController } from "@ionic/angular";
+import { identity } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
   constructor(public ToastController: ToastController) {}
 
-  async exibirToast(frete: any){
+  async exibirToast(frete: any) {
     const toast = await this.ToastController.create({
       message: "O seu frete normal é: R$" + frete,
-      duration: 2000
-    })
+      duration: 2000,
+    });
     toast.present();
   }
 
-  distancia: any = '';
-  peso: any = '';
-  quantidade: any = '';
-  tipo: any = '';
-  frete: any = '';
+  administrador = true;
+  distancia: any = "";
+  peso: any = "";
+  quantidade: any = "";
+  tipo: any = "";
+  frete: any = "";
   entrega: any = {
     express: 0.1,
-    agendada: 0.2
+    agendada: 0.2,
   };
   desconto: any = {
     ate3: 0.1,
     ate5: 0.15,
     ate10: 0.2,
-    acimaDe11: 0.25
-  }
+    acimaDe11: 0.25,
+  };
 
-  km = 2.5;
-  kg = 1.2;
+  km = 1.0;
+  kg = 0.5;
 
   calcular() {
     this.entrega = {
       express: 0.1,
-      agendada: 0.2
+      agendada: 0.2,
     };
+
+    if(this.peso <= 1){
+      this.kg = 0.2
+    }else if(this.peso >= 2 && this.peso <= 5){
+      this.kg = 0.3
+    }else if(this.peso >= 6 && this.peso <= 10){
+      this.kg = 0.4
+    }else if(this.peso >= 11 && this.peso <= 15){
+      this.kg = 0.5
+    }else{
+      this.kg = 0.6
+    }
+
+    if(this.distancia >= 100 && this.distancia <= 500){
+      this.km = 0.15
+    }else if(this.distancia >= 501 ){
+      this.km = 0.12
+    }
 
     this.frete = this.km * this.distancia + this.kg * this.peso;
 
-    if(this.quantidade <= 3){
-      this.frete = this.frete - (this.frete * this.desconto.ate3)
-    }else if(this.quantidade >= 4 && this.quantidade <= 5 ){
-      this.frete = this.frete - (this.frete * this.desconto.ate5)
-    }else if(this.quantidade >= 6 && this.quantidade <= 10 ){
-      this.frete = this.frete - (this.frete * this.desconto.ate10)
-    }else if(this.quantidade >= 11){
-      this.frete = this.frete - (this.frete * this.desconto.acimaDe11)
+    // this.frete = parseFloat(this.frete)
+
+    if (this.quantidade <= 3) {
+      this.frete = this.frete - this.frete * this.desconto.ate3;
+    } else if (this.quantidade >= 4 && this.quantidade <= 5) {
+      this.frete = this.frete - this.frete * this.desconto.ate5;
+    } else if (this.quantidade >= 6 && this.quantidade <= 10) {
+      this.frete = this.frete - this.frete * this.desconto.ate10;
+    } else if (this.quantidade >= 11) {
+      this.frete = this.frete - this.frete * this.desconto.acimaDe11;
     }
 
-    this.entrega.express =parseFloat( this.frete + (this.frete * this.entrega.express))
-    this.entrega.agendada = parseFloat(this.frete + (this.frete * this.entrega.agendada))
-
+    this.entrega.express = this.frete + this.frete * this.entrega.express;
+    this.entrega.agendada = this.frete + this.frete * this.entrega.agendada;
 
     this.exibirToast(this.frete);
   }
+
 }
